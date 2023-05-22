d6e7b2770b2fcc874b05d65ede20614e9b94fa5b
Bah Amadou <bah.amadouth96@gmail.com>
2023-05-22 19:39:46 +0200

    MIN-31 add minimal frontend setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,18 @@
+import "@/styles/dist.css";
+import "focus-visible";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Minkadi App",
+  description: "Minkadi App",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Minkadi - fast and reliable.</title>
+        <meta
+          name="description"
+          content="By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to send to avoid painful losses."
+        />
+      </Head>
+      <main></main>
+    </>
+  );
+}
