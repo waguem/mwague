4a000cd1e6086de943f221b3a19c072fdadefee0
Bah Amadou <bah.amadouth96@gmail.com>
2024-05-09 10:13:03 +0200

    MWG-1 enable image build

--- backend/mkdi_backend/auth/auth.py
+++ backend/mkdi_backend/auth/auth.py
@@ -14,9 +14,9 @@ def create_access_token(data: dict) -> str:
     expires_delta = timedelta(minutes=settings.AUTH_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.AUTH_SECRET, algorithm=settings.AUTH_ALGORITHM)
     return encoded_jwt
