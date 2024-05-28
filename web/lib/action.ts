fd9581828d324f8756eeefa4f38a9405137fa1ff
Bah Amadou <bah.amadouth96@gmail.com>
2024-05-28 19:14:25 +0200

    update

--- web/lib/action.ts
+++ web/lib/action.ts
@@ -2,6 +2,5 @@ import { signIn } from "@/auth";
 
 export const loginAction = async () => {
   "use server";
-  const redirectUri = process.env.WEB_DOMAIN ? `${process.env.WEB_DOMAIN}/dashboard` : "/dashboard";
-  await signIn("keycloak", { redirectTo: redirectUri });
+  await signIn("keycloak", { redirectTo: "/dashboard" });
 };
