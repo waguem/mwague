817711cb851f7886421d34968e42c1ae60fe2d77
Bah Amadou <bah.amadouth96@gmail.com>
2024-06-22 15:52:09 +0200

    update

--- web/app/dashboard/activity/loading.tsx
+++ web/app/dashboard/activity/loading.tsx
@@ -1,8 +1,17 @@
 import LoaderSkeleton from "@/components/layouts/LoaderSkelleton";
-import React from "react";
 
-const loading = () => {
-  return <LoaderSkeleton />;
-};
+LoaderSkeleton;
+export default function Loading() {
+  return (
+    <div className="space-y-4">
+      <div className="text-xl font-medium text-zinc-500">Loading...</div>
 
-export default loading;
+      <div className="grid grid-cols-3 gap-6">
+        <LoaderSkeleton />
+        <LoaderSkeleton />
+        <LoaderSkeleton />
+        <LoaderSkeleton />
+      </div>
+    </div>
+  );
+}
