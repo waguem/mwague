c2bff34f1277029a38608c39145df92c26a4d908
Bah Amadou <bah.amadouth96@gmail.com>
2024-07-03 17:58:28 +0200

    update networks

--- compose/runcitests.sh
+++ compose/runcitests.sh
@@ -3,7 +3,7 @@ echo "Running backend unitests"
 #docker exec -it mkdi-backend pytest /backend
 echo "Running functional tests"
 #docker exec -it mkdi-backend pytest /mkdi-shared
-docker exec -it bdd /run.sh http://backend:8080/api/v1/ping
+docker exec -i bdd /run.sh http://backend:8080/api/v1/ping
 
 # show result
 echo "[TESTS RESULT]: $?";
