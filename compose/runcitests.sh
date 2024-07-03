0a0c9d3d2aa16c8a2a8723239982077ba83c5591
Bah Amadou <bah.amadouth96@gmail.com>
2024-07-04 01:07:49 +0200

    update

--- compose/runcitests.sh
+++ compose/runcitests.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 echo "Running backend unitests"
 echo "Running functional tests"
-docker exec -i bdd /run.sh http://backend-ci:8080/api/v1/ping
+docker exec -i bdd /run.sh http://backend-ci:80/api/v1/ping
 
 success=$?
 # show result
